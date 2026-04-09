ef1c22068d67dae57c8e0f492f903cf4a496f3b1
DJSurgeon <djsurgeon83@gmail.com>
2026-04-09 11:53:13 +0200

    refactor(structure): Check consistency and correct renderin main page

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import './app/styles/global.css';
 
 const root = document.getElementById("root");
 if (root) {
